0d2aadb186fbf918f12312cb12802fcde5603823
Ankit Meena <ankitmeena9783037175@gmail.com>
2024-10-06 09:27:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Fyle Backend Challenge
+
+## Who is this for?
+
+This challenge is meant for candidates who wish to intern at Fyle and work with our engineering team. You should be able to commit to at least 6 months of dedicated time for internship.
+
+## Why work at Fyle?
+
+Fyle is a fast-growing Expense Management SaaS product. We are ~40 strong engineering team at the moment. 
+
+We are an extremely transparent organization. Check out our [careers page](https://careers.fylehq.com) that will give you a glimpse of what it is like to work at Fyle. Also, check out our Glassdoor reviews [here](https://www.glassdoor.co.in/Reviews/Fyle-Reviews-E1723235.htm). You can read stories from our teammates [here](https://stories.fylehq.com).
+
+
+## Challenge outline
+
+**You are allowed to use any online/AI tool such as ChatGPT, Gemini, etc. to complete the challenge. However, we expect you to fully understand the code and logic involved.**
+
+This challenge involves writing a backend service for a classroom. The challenge is described in detail [here](./Application.md)
+
+
+## What happens next?
+
+You will hear back within 48 hours from us via email. 
+
+
+## Installation
+
+1. Fork this repository to your github account
+2. Clone the forked repository and proceed with steps mentioned below
+
+### Install requirements
+
+```
+virtualenv env --python=python3.8
+source env/bin/activate
+pip install -r requirements.txt
+```
+### Reset DB
+
+```
+export FLASK_APP=core/server.py
+rm core/store.sqlite3
+flask db upgrade -d core/migrations/
+```
+### Start Server
+
+```
+bash run.sh
+```
+### Run Tests
+
+```
+pytest -vvv -s tests/
+
+# for test coverage report
+# pytest --cov
+# open htmlcov/index.html
+```
+
+## Building and running the application with Docker
+To build and run the Flask application using Docker, follow these steps:
+
+Clone this repository to your local machine.
+### Navigate to the project directory:
+`cd fyle-interview-intern-backend`
+
+
+### Build the Docker image:
+`docker build -t flask-app .`
+
+### Run the Docker container:
+`docker run -p 7755:7755 flask-app`
+
+This will start the Flask application, and it will be accessible at http://localhost:7755.
+
+
+## Building and Running with Docker Compose
+To build and run the Flask application using Docker Compose, follow these steps:
+
+Clone this repository to your local machine.
+### Navigate to the project directory:
+`cd fyle-interview-intern-backend`
+
+### Run the Docker Compose command: 
+`docker-compose up` 
+
+This will build the Docker image and start the Flask application, accessible at `http://localhost:7755`.
+
+### To stop the Docker Compose services, press Ctrl + C, and then run:
+`docker-compose down` 
+
+This will stop and remove the containers, networks, and volumes created by docker-compose up.
---
+++ tests/teachers_test.py
@@ -0,0 +1,157 @@
+from core.libs.exceptions import FyleError
+from core.models.assignments import AssignmentStateEnum, GradeEnum
+
+
+def test_get_assignments_teacher_1(client, h_teacher_1):
+    response = client.get(
+        '/teacher/assignments',
+        headers=h_teacher_1
+    )
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    for assignment in data:
+        assert assignment['teacher_id'] == 1
+
+
+def test_get_assignments_teacher_2(client, h_teacher_2):
+    response = client.get(
+        '/teacher/assignments',
+        headers=h_teacher_2
+    )
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    for assignment in data:
+        assert assignment['teacher_id'] == 2
+        assert assignment['state'] in ['SUBMITTED', 'GRADED']
+
+
+def test_grade_assignment_cross(client, h_teacher_2):
+    """
+    failure case: assignment 1 was submitted to teacher 1 and not teacher 2
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_2,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+
+
+def test_grade_assignment_bad_grade(client, h_teacher_1):
+    """
+    failure case: API should allow only grades available in enum
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "AB"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'ValidationError'
+
+
+def test_grade_assignment_bad_assignment(client, h_teacher_1):
+    """
+    failure case: If an assignment does not exists check and throw 404
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 100000,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 404
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+
+
+def test_grade_assignment_draft_assignment(client, h_teacher_1):
+    """
+    failure case: only a submitted assignment can be graded
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1
+        , json={
+            "id": 2,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+
+
+def test_grade_submitted_assignment(client, h_teacher_2):
+    """
+    Test case for grading correct assignment
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_2,
+        json={
+            "id": 3,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 200
+    data = response.json
+    assert data['data']['state'] == AssignmentStateEnum.GRADED.value
+    assert data['data']['grade'] == GradeEnum.A
+
+
+def test_regrade_graded_assignment(client, h_teacher_2):
+    """
+    Test case for regrading an already graded assignment
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_2,
+        json={
+            "id": 3,
+            "grade": "C"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+    assert data['error'] == 'FyleError'
+    assert data['message'] == 'Cannot grade an already graded assignment'
+
+
+def test_get_assignments_for_invalid_teacher(client, h_invalid_teacher):
+    """
+    Test case for an invalid teacher
+    """
+    response = client.get(
+        '/teacher/assignments',
+        headers=h_invalid_teacher
+    )
+
+    assert response.status_code == 404
+    assert response.json['error'] == 'FyleError'
+    assert response.json['message'] == 'Teacher with given id does not exist'
